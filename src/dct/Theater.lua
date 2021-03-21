2dcaf356e1ae275a896c67d0579524c66aae7b31
Jonathan Toppins <jtoppins@users.noreply.github.com>
2021-03-20 23:59:33 -0400

    [wip] region: create a region manager
    This change moves everything to do with region management under one
    class instead of it being sprinkled between the Theater and Region
    classes. This also change to using the system model added to the
    Theater class, so initilization of regions and generation of a new
    theater becomes trivial.

--- src/dct/Theater.lua
+++ src/dct/Theater.lua
@@ -7,7 +7,6 @@
 require("os")
 require("io")
 require("lfs")
-local utils       = require("libs.utils")
 local containers  = require("libs.containers")
 local json        = require("libs.json")
 local enum        = require("dct.enum")
@@ -16,7 +15,6 @@ local Observable  = require("dct.libs.Observable")
 local uicmds      = require("dct.ui.cmds")
 local STM         = require("dct.templates.STM")
 local Template    = require("dct.templates.Template")
-local Region      = require("dct.templates.Region")
 local AssetManager= require("dct.assets.AssetManager")
 local Commander   = require("dct.ai.Commander")
 local Command     = require("dct.Command")
@@ -105,7 +103,6 @@ function Theater:__init()
 	end
 
 	self:loadSystems()
-	self:loadRegions()
 	self:queueCommand(5, Command(self.__clsname..".delayedInit",
 		self.delayedInit, self))
 	self:queueCommand(100, Command(self.__clsname..".export",
@@ -158,26 +155,11 @@ function Theater:loadSystems()
 		"dct.systems.bldgPersist",
 		"dct.systems.weaponstracking",
 		"dct.systems.blasteffects",
+		"dct.templates.RegionManager",
 	}
 
 	for _, syspath in ipairs(systems) do
 		self:addSystem(syspath)
-	end
-end
-
-function Theater:loadRegions()
-	for filename in lfs.dir(settings.theaterpath) do
-		if filename ~= "." and filename ~= ".." and
-			filename ~= ".git" and filename ~= "settings" then
-			local fpath = settings.theaterpath..utils.sep..filename
-			local fattr = lfs.attributes(fpath)
-			if fattr.mode == "directory" then
-				local r = Region(fpath)
-				assert(self.regions[r.name] == nil, "duplicate regions " ..
-					"defined for theater: " .. settings.theaterpath)
-				self.regions[r.name] = r
-			end
-		end
 	end
 end
 
@@ -230,9 +212,7 @@ function Theater:loadOrGenerate()
 		self:getAssetMgr():unmarshal(self.statetbl.assetmgr)
 	else
 		Logger:info("generating new theater")
-		for _, r in pairs(self.regions) do
-			r:generate(self.assetmgr)
-		end
+		self:getRegionMgr():generate()
 		-- TODO: temperary, spawn all generated assets
 		-- eventually we will want to spawn only a set of assets
 		local assetnames = self.assetmgr:filterAssets(function()
@@ -363,6 +343,10 @@ function Theater:export(_)
 	statefile:flush()
 	statefile:close()
 	return self.savestatefreq
+end
+
+function Theater:getRegionMgr()
+	return self:getSystem("dct.templates.RegionManager")
 end
 
 function Theater:getAssetMgr()
